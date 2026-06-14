418d193c496c66d709fe217ec54b1913caf84cbd
Aditya <uajavha@gmail.com>
2026-06-15 01:02:37 +0530

    feat: add Google Analytics, site verification, and sitemap integration to layout and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   base: './',
+  site: 'https://www.convertify.pro',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
